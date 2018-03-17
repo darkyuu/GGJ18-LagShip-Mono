0b159271b780c4c5a098ee4dc603b6014b3376ba
Durongrit Hirunked <darkyuu@hotmail.com>
2018-03-17 13:37:27 +0700

    ADD: left, right and shoot command

---
+++ Scripts/TurnLeftCommandObject.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public class TurnLeftCommandObject : RigidBody2D
+{
+    public override void _Ready()
+    {
+
+    }
+}
---
+++ Scripts/TurnRightCommandObject.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public class TurnRightCommandObject : RigidBody2D
+{
+    public override void _Ready()
+    {
+
+    }
+}
